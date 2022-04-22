4e2e2205e145ab334a7b7368b9026aae3df91856
niushufeng <45967122+niushufeng@users.noreply.github.com>
2022-04-22 21:23:51 +0800

    Update product_csv.py

--- code/product_csv.py
+++ code/product_csv.py
@@ -28,3 +28,21 @@ frame = pd.DataFrame({'x_100':x_100})
 frame.to_csv("x_100.csv",index = False , sep = ',')
 frame = pd.DataFrame({'x_1day':x_1day})
 frame.to_csv("x_1day.csv",index = False , sep = ',')
+
+sns.kdeplot(x_1, shade=False, bw=0.005, color="c",linestyle='-',marker=r'v',label='$\Delta t$= 1min') #核密度
+sns.kdeplot(x_5, shade=False, bw=0.01, color="b",linestyle='-',marker=r'^',label='$\Delta t$= 5min') #核密度
+sns.kdeplot(x_10, shade=False, bw=0.01, color="r",linestyle='-',marker=r'<',label='$\Delta t$= 10min') #核密度
+sns.kdeplot(x_100, shade=False, bw=0.0135, color='m',linestyle='-',marker=r'>',label='$\Delta t$= 100min') #核密度
+sns.kdeplot(x_1day, shade=False, bw=0.011, color='#B22222',cut=0,linestyle='-',marker=r'+',label='$\Delta t$= 1day') #核密度
+plt.yscale('log') # 纵坐标转换
+
+plt.xlabel('$G_{\Delta t}(t) = \ln S(t + \Delta t) - \ln S(t)$')
+plt.ylabel('$\log_{10}P(G)/PDF$')
+plt.savefig('1-1440(400w).pdf',bbox_inches='tight')
+plt.rcParams['axes.unicode_minus'] = False
+plt.legend(loc='best')
+
+plt.show()
+
+plt.plot(x_100) # 2013年收益率波动大，中间
+
